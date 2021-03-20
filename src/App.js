6b37469d0e4e4eefa5b2eb29c4d0900469c32ffc
Faerque <202001512@eastdelta.edu.bd>
2021-03-20 19:55:52 +0600

    all done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React, { createContext, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Blog from "./components/Blog/Blog";
+import Contact from "./components/Conatact/Contact";
+import Destination from "./components/Destination/Destination";
+import Header from "./components/Header/Header";
+import Home from './components/Home/Home';
+import Login from "./components/Login/Login";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import SelectDest from "./components/SelectDest/SelectDest";
+
+export const UserContext = createContext()
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+         <Router> 
+          <Header/>
+          <Switch>
+              <Route path="/home">
+                <Home/>  
+              </Route>
+              <PrivateRoute exact path="/destination/:id">
+                  <SelectDest/> 
+              </PrivateRoute>
+              <PrivateRoute exact path="/destination/:id/:placeFrom/:placeTo">
+              <Destination />
+              </PrivateRoute>
+              <Route path="/blog">
+                  <Blog/>
+              </Route>
+              <Route path="/contact">
+                  <Contact/>
+              </Route>
+              <Route path="/login">
+                    <Login/>
+              </Route>
+              <Route exact path="/">
+                <Home/>
+              </Route>
+          </Switch>
+        </Router>
+      </UserContext.Provider>
   );
 }
 
